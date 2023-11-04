a2baffd2cd27f8498c1ca6071c28c97217ae8cc4
TMCreme <tonnybright123@yahoo.com>
2023-11-04 20:06:45 +0000

    blog blueprint

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -5,11 +5,11 @@ import functools
 
 from flask import (
     Blueprint, request, redirect, url_for, flash, render_template,
-    session
+    session, g
 )
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from db import get_db
+from .db import get_db
 
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
@@ -32,7 +32,7 @@ def register():
             try:
                 db.execute(
                     "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password))
+                    (username, password)
                 )
                 db.commit()
             except db.IntegrityError:
@@ -67,3 +67,31 @@ def login():
             return redirect(url_for("index"))
         flash(error)
     return render_template("auth/login.html")
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    """Check if the request is from a logged in user"""
+    user_id = session.get("user_id", None)
+    db = get_db()
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = db.execute(
+            'SELECT * FROM user WHERE id = ?', (user_id, )
+        ).fetchone()
+
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        return view(**kwargs)
+    return wrapped_view
